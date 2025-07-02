735ec344607ac3dd924014f71b33b152cab8eee5
elianacris <elianacrisdeus@outlook.com>
2025-07-01 23:45:30 -0300

    remove componentes FAQ e Divider, incluindo estilos e referências

--- src/app/FAQ/FAQ.tsx
+++
@@ -1,8 +0,0 @@
-function FAQ(){
-    return (
-        <div>
-
-        </div>
-    )
-}
-export default FAQ;
--- src/components/Divider/Divider.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Divider.module.scss';
-
-type DividerProps = {
-  className?: string;
-};
-
-export default function Divider({ className = '' }: DividerProps) {
-  return <hr className={`${styles.divider} ${className}`} />;
-}
